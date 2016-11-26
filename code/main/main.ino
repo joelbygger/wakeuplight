e3c0bb976e895db24900a2f0dbb57a6f48c06df1
Joels Dator <joel.sael@gmail.com>
2016-11-26 23:00:06 +0100

    testi testi

--- code/main/main.ino
+++ code/main/main.ino
@@ -7,12 +7,21 @@
 // Clock.
 const int intPinPWMOutput = 10; // 16 bit PWM channel, if configured so.
 const int intPinPWMInput = 2;
-int intCounter = 0;             // counts rising edge clock signals
-int intCompensator = 0;         // compensates for clock inaccuracy, freq. measured to be: 490.2Hz
+static volatile int priv_intCounter = 0;             // counts rising edge clock signals
+static volatile int priv_intCompensator = 0;         // compensates for clock inaccuracy, freq. measured to be: 490.2Hz
 const int intComepnsatorLimit = 5;
-int intSeconds = 0;
-int intMinutes = 0;
-int intHours = 0;
+typedef struct
+{
+    int seconds;
+    int minutes;
+    int hours;
+}time_t;
+static time_t priv_currTime;
+static bool priv_time_activateCursor = false;
+static bool priv_time_moveCursor = false;
+const unsigned long cursor_disable_timeout = 6000; // [ms]
+// We seem to need this, otherwise cursor will flicker.
+const unsigned long cursor_update_interval = 100; // [ms]
 
 // Display
 const int led_pin_rs = 9;
@@ -28,27 +37,31 @@ const int ledPin = 3;
 // Joystick.
 typedef enum
 {
-    sw_press_none,
-    sw_press_short,
-    sw_press_long,
-    sw_press_setTime
+    sw_press_typeNone,
+    sw_press_veryLongDelay,
+    sw_press_longDelay,
+    sw_press_shortDelay,
+    sw_press_unparsable
 } joySWstates_t;
 
 const int joyPinX = A0;
 const int joyPinY = A1;
 const int joySWpin = 7;
-bool joyShouldReadX = true;
-int joyReadX = 0;
-int joyReadY = 0;
-unsigned long joyReadAnalogDelay_last = 0;
-unsigned long joySWdebounceTime_last = 0xFFFFFFFFFFFFFFFFFFFFFFFF;
-joySWstates_t joySWpress = sw_press_none;
+static int joyReadX = 0;
+static int joyReadY = 0;
+static volatile unsigned long priv_joySWdebounceTime_last = 0xFFFFFFF;
+static volatile unsigned long priv_lastTimeJoyMovement = 0;
+volatile joySWstates_t priv_joySWpress = sw_press_typeNone;
+
 
 const unsigned long JOY_READ_ANALOG_DELAY = 100; // [ms], we don't want to read too often.
 // The times seems to be difficult to set, so use big differences.
-const unsigned long JOY_SW_SHORT_DELAY = 100; // [ms]
+const unsigned long JOY_SW_SHORT_DELAY = 60; // [ms] I am not usre if this is real, sounds like short time, but works better than 100.
 const unsigned long JOY_SW_LONG_DELAY = 1000; // [ms] 
-const unsigned long JOY_SW_VERY_LONG_DELAY = 3000; // [ms] 
+const unsigned long JOY_SW_VERY_LONG_DELAY = 2000; // [ms] 
+// [ADC] Smaller movements than this we don't count, abs val.
+const int JOY_MIN_DIFF_TO_COUNT = 200;
+const int JOY_IN_REST = 501; // Pretty much true, we have 2048 bits ADC.
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(led_pin_rs, led_pin_en, led_pin_d4, led_pin_d5, led_pin_d6, led_pin_d7);
@@ -58,12 +71,23 @@ LiquidCrystal lcd(led_pin_rs, led_pin_en, led_pin_d4, led_pin_d5, led_pin_d6, le
 */
 void setup()
 {
+    Serial.begin(57600);
+    while (!Serial) 
+    {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+    Serial.println("Connected, we begin!");
+
     // Display.
     lcd.begin(16, 2); // set up the LCD's number of columns and rows.
     lcd.clear(); // start with a blank screen.
     lcd.setCursor(0,0); // set cursor to column 0, row 0 (the first row).
     
     // Clock.
+    priv_currTime.seconds = 0;
+    priv_currTime.minutes = 0;
+    priv_currTime.hours = 0;
+    priv_lastTimeJoyMovement = 0;
     //  intPinPWMInput is our interrupt, IntClockCounter function is called when
     //  invoked on a RISING clock edge
     attachInterrupt(digitalPinToInterrupt(intPinPWMInput), IntClockCounter, RISING);
@@ -85,20 +109,20 @@ void setup()
 */
 void IntClockCounter()      // called by interrupt
 {
-    intCounter ++;        // with each clock rise add 1 to intCounter count
+    priv_intCounter++;
 
-    if (intCompensator >= intComepnsatorLimit)
+    if (priv_intCompensator >= intComepnsatorLimit)
     {
-        intCompensator = 0;
-        intCounter--;
+        priv_intCompensator = 0;
+        priv_intCounter--;
     }
     
-    if (intCounter == 490) // 490Hz reached
+    if (priv_intCounter == 490) // 490Hz reached
     {  
-        intCompensator++;
+        priv_intCompensator++;
     
-        intSeconds ++;          // after one 490Hz cycle add 1 second ;)
-        intCounter = 0;     // Reset after 1 second is reached
+        priv_currTime.seconds ++;          // after one 490Hz cycle add 1 second ;)
+        priv_intCounter = 0;     // Reset after 1 second is reached
     }
     
     return;
@@ -134,31 +158,148 @@ void printTime(String txt, int hrs, int mins, int secs)
 /**
 * Upcates and compaensates clock.
 */
-void updateClock()
+void updateTime(time_t &time)
 {
-    if (intSeconds == 60)
+    if (time.seconds >= 60)
     {
-        intMinutes++;
-        intSeconds = 0;
+        time.minutes++;
+        time.seconds = 0;
     }
-    if(intMinutes == 60)
+    if(time.minutes >= 60)
     {
-        intHours++;
-        intMinutes = 0;
+        time.hours++;
+        time.minutes = 0;
     }
-    if(intHours == 24)
+    if(time.hours >= 24)
     {
-        intHours = 0;
+        time.hours = 0;
+    }
+}
+
+/**
+ * Returns next cursor position.
+ */
+int doMoveCursor(const int currCursorPos)
+{
+    const int hourPos = 7;
+    const int minPos = 10;
+    const int secPos = 13;
+    int result = currCursorPos;
+
+    switch(currCursorPos)
+    {
+        case hourPos:
+            result = minPos;
+            break;
+        case minPos:
+            result = secPos;
+            break;
+        case secPos:
+        default:
+            // Handle all strange stuff as hour pos.
+            result = hourPos;
+            break;
+    }
+
+    return result;
+}
+
+/**
+ * Updates cursor pos, moves cursor and 
+ * deactivates curor if no movement for too long time.
+ */
+void updateCursorPos(bool &cursorActive, bool &moveCursor)
+{
+    static int cursorPos = 0;
+    static bool firstExec = true;
+    static unsigned long lastUpdateTime = 0;
+    const unsigned long currTime = millis();
+    static bool lastCallCursorWasActive = false;
+
+    // Some init.
+    if (firstExec)
+    {
+        lastUpdateTime = currTime;
+        firstExec = false;
+        cursorPos = doMoveCursor(255); // Some strange number to reset position.
+    }
+
+    // Shall cursor be deactivated due to timeout?
+    if (cursorActive &&
+        ((currTime - priv_lastTimeJoyMovement) > cursor_disable_timeout))
+    {
+        cursorActive = false;
+    }
+
+    // If we become active, set cursor to start pos.
+    if (!lastCallCursorWasActive && cursorActive)
+    {
+        cursorPos = doMoveCursor(255); // Some strange number to reset position.
+    }
+    lastCallCursorWasActive = cursorActive;
+
+    if((currTime - lastUpdateTime) > cursor_update_interval)
+    {
+        if (cursorActive)
+        {
+            if (moveCursor)
+            {
+                lcd.setCursor(cursorPos, 1);
+                lcd.write(' ');
+                cursorPos = doMoveCursor(cursorPos);
+                moveCursor = false;
+            }
+
+            lcd.setCursor(cursorPos, 1);
+            lcd.write('I');
+            lastUpdateTime = currTime;
+
+            Serial.print("currTime ");
+            Serial.print(currTime);
+            Serial.print(" priv_lastTimeJoyMovement ");
+            Serial.print(priv_lastTimeJoyMovement);
+            Serial.print(" diff ");
+            Serial.println(currTime - priv_lastTimeJoyMovement);
+        }
+        else
+        {
+            Serial.println("Disable cursor");
+            lcd.setCursor(cursorPos, 1);
+            lcd.write(' ');
+        }
     }
 }
 
 /**
 * Updates display.
 */
-void updateDisplay(int hrs, int mins, int secs, const bool clockCanBeChanged)
-{
+void updateDisplay(const time_t time)
+{   
     lcd.setCursor(0, 0);
-    printTime("Time: ", hrs, mins, secs);
+    printTime("Time: ", time.hours, time.minutes, time.seconds);
+}
+
+/**
+ * Returns true if movement was so big we count is as a movement.
+ */
+bool readJoyMovmentCounts(const int last, const int curr)
+{
+    bool result = false;
+
+    if(last > curr)
+    {
+        result = (last - curr) > JOY_MIN_DIFF_TO_COUNT ? true : false;
+    }
+    else if(last < curr)
+    {
+        result = (curr- last) > JOY_MIN_DIFF_TO_COUNT ? true : false;
+    }
+    else
+    {
+        result = false;
+    }
+
+    return result;
 }
 
 /**
@@ -166,20 +307,43 @@ void updateDisplay(int hrs, int mins, int secs, const bool clockCanBeChanged)
  */
 void readJoy()
 {
-    if ( (millis() - joyReadAnalogDelay_last) > JOY_READ_ANALOG_DELAY )
+    static bool shouldReadX = true;
+    static unsigned long lastReadMilli = 0;
+    static int lastReadX = 0;
+    static int lastReadY = 0;
+
+    if ( (millis() - lastReadMilli) > JOY_READ_ANALOG_DELAY )
     {
-        if (joyShouldReadX)
+        if (shouldReadX)
         {
             joyReadX = analogRead(joyPinX);
+
+            if (readJoyMovmentCounts(lastReadX, joyReadX) ||
+                (joyReadX > (JOY_IN_REST + JOY_MIN_DIFF_TO_COUNT)) ||
+                (joyReadX < (JOY_IN_REST - JOY_MIN_DIFF_TO_COUNT)))
+            {
+                priv_lastTimeJoyMovement = millis();
+            }
+
+            lastReadX = joyReadX;
         }
         else
         {
             joyReadY = analogRead(joyPinY);
+
+            if (readJoyMovmentCounts(lastReadY, joyReadY) ||
+                (joyReadY > (JOY_IN_REST + JOY_MIN_DIFF_TO_COUNT)) ||
+                (joyReadY < (JOY_IN_REST - JOY_MIN_DIFF_TO_COUNT)))
+            {
+                priv_lastTimeJoyMovement = millis();
+            }
+
+            lastReadY = joyReadY;
         }
 
-        joyShouldReadX = !joyShouldReadX;
-        joyReadAnalogDelay_last = millis();
-    }   
+        shouldReadX = !shouldReadX;
+        lastReadMilli = millis();
+    }
 }
 
 /**
@@ -188,30 +352,50 @@ void readJoy()
 void joySWpressed()
 {
     unsigned long currTime = millis();
-    unsigned long timeDiff = currTime - joySWdebounceTime_last;
+    unsigned long timeDiff = currTime - priv_joySWdebounceTime_last;
+    priv_lastTimeJoyMovement = millis();
 
-    // We are only interessed in presses, i.e. signal is low.
-    if (digitalRead(joySWpin) == LOW)
+    if (timeDiff < JOY_SW_SHORT_DELAY)
     {
-        joySWdebounceTime_last = currTime;
+        // Ignore, treat it as debounce.
+        Serial.print("Debounce ");
+        Serial.println(timeDiff);
+    }
+    else if (digitalRead(joySWpin) == LOW)
+    {
+        // Pressed, i.e. signal is low.
+        Serial.print("Depressed " );
+        Serial.println(currTime);
+        priv_joySWdebounceTime_last = currTime;
     }
     else
     {
-        if (timeDiff > JOY_SW_VERY_LONG_DELAY)
+        // Released.
+        Serial.print("Release " );
+        Serial.print(currTime);
+        if (timeDiff >= JOY_SW_VERY_LONG_DELAY)
         {
-            joySWpress = sw_press_setTime;
+            Serial.print(" Very long ");
+            Serial.println(timeDiff);
+            priv_joySWpress = sw_press_veryLongDelay;
         }
-        else if (timeDiff > JOY_SW_LONG_DELAY)
+        else if (timeDiff >= JOY_SW_LONG_DELAY)
         {
-            joySWpress = sw_press_long;
+            Serial.print(" Long ");
+            Serial.println(timeDiff);
+            priv_joySWpress = sw_press_longDelay;
         }
-        else if (timeDiff > JOY_SW_SHORT_DELAY)
+        else if (timeDiff >= JOY_SW_SHORT_DELAY)
         {
-            joySWpress = sw_press_short;
+            Serial.print(" Short ");
+            Serial.println(timeDiff);
+            priv_joySWpress = sw_press_shortDelay;
         }
         else
         {
-            joySWpress = sw_press_none;
+            Serial.print(" Unparsable ");
+            Serial.println(timeDiff);
+            priv_joySWpress = sw_press_unparsable;
         }
     }   
 }
@@ -221,35 +405,36 @@ void joySWpressed()
 */
 void loop()
 {
-    static bool clockCanBeChanged = false;
-
     readJoy();
   
-    updateClock();
+    updateTime(priv_currTime);
 
-    updateDisplay(intHours, intMinutes, intSeconds, clockCanBeChanged);
+    updateCursorPos(priv_time_activateCursor, priv_time_moveCursor);
+    updateDisplay(priv_currTime);
 
-    switch (joySWpress)
+    switch (priv_joySWpress)
     {
-        case sw_press_none:
-            analogWrite(ledPin, 0);
-            clockCanBeChanged = false;
+        case sw_press_typeNone:
+            // Do nothing.
             break;
-        case sw_press_short:
-            analogWrite(ledPin, 10);
+        case sw_press_veryLongDelay:
             break;
-        case sw_press_long:
-            analogWrite(ledPin, 255);
+        case sw_press_longDelay:
+            priv_time_activateCursor = !priv_time_activateCursor;
             break;
-        case sw_press_setTime:
-            clockCanBeChanged = true;
+        case sw_press_shortDelay:
+            priv_time_moveCursor = true;
+            break;
+        case sw_press_unparsable:
             break;
         default:
             // Do nothing
             break;
     }
 
-    if (clockCanBeChanged)
+    priv_joySWpress = sw_press_typeNone;
+
+    //if (clockCanBeChanged)
     {
         if (joyReadX > 1000)
         {
@@ -260,45 +445,4 @@ void loop()
             analogWrite(ledPin, 0);
         }
     }
-
-    /*if (intSeconds % 10 == 0)
-    {
-        analogWrite(ledPin, 20);
-    }
-    else if (intSeconds % 10 == 1)
-    {
-        analogWrite(ledPin, 60);
-    }
-    else if (intSeconds % 10 == 2)
-    {
-        analogWrite(ledPin, 100);
-    }
-    else if (intSeconds % 10 == 3)
-    {
-        analogWrite(ledPin, 160);
-    }
-    else if (intSeconds % 10 == 4)
-    {
-        analogWrite(ledPin, 256);
-    }
-    else if (intSeconds % 10 == 5)
-    {
-        analogWrite(ledPin, 255);
-    }
-    else if (intSeconds % 10 == 6)
-    {
-        analogWrite(ledPin, 200);
-    }
-    else if (intSeconds % 10 == 7)
-    {
-        analogWrite(ledPin, 160);
-    }
-    else if (intSeconds % 10 == 8)
-    {
-        analogWrite(ledPin, 100);
-    }
-    else if (intSeconds % 10 == 9)
-    {
-        analogWrite(ledPin, 1);
-    }*/
 }
