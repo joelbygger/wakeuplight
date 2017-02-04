3d3cea3816b0ed25e05d1f920e20c8f1d97ff69f
Joels Dator <joel.sael@gmail.com>
2017-02-04 10:42:23 +0100

    LED is now on at full effect 20 minutes after has finished fading.

--- code/main/led.ino
+++ code/main/led.ino
@@ -9,13 +9,16 @@ typedef enum
 
 const uint32_t ALARM_START_TIME_HOUR = 6;
 const uint32_t ALARM_START_TIME_MIN = 0;
-
+// The alarm minutes counter does not wrap at 60, so this digit can be bigger.
+const uint32_t MINUTES_WITH_FULL_LIGHT = 20;
+// The alarm minutes counter does not wrap at 60, so this array can be bigger.
 const unsigned int ledLinearity[31] = {
     0, 1, 2, 3, 4, 6, 8, 10, 13, 16, 
     20, 24, 29, 34, 40, 47, 54, 63, 72, 82, 
     92, 104, 116, 130, 145, 160, 177, 195, 214, 234, 
     255
 };
+const uint32_t LED_FADE_TIME = sizeof(ledLinearity)/sizeof(ledLinearity[0]);
 
 
 
@@ -35,10 +38,11 @@ void led_controlLED(const time_t time)
                 (time.minutes == ALARM_START_TIME_MIN) &&
                 time.dayNo <= 4) // Only alarm on weekdays.
             {
-                Serial.print("Alarm started\n");
-                Serial.print((sizeof(ledLinearity)/sizeof(ledLinearity[0])));
+                Serial.print("Alarm started at time: ");
+                Serial.println(time.seconds);
                 alarmMinute = 0;
                 int pwmVal_ = ledLinearity[alarmMinute];
+                Serial.println(pwmVal_);
                 analogWrite(ledPin, pwmVal_);
                 alarmMinute++;
                 lastAlarmMinute = time.minutes;
@@ -51,24 +55,39 @@ void led_controlLED(const time_t time)
         {
             if (lastAlarmMinute != time.minutes)
             {
-                Serial.print(" new alarm minute: ");
-                Serial.print(alarmMinute);
-                Serial.print("\n");
-                int pwmVal_ = ledLinearity[alarmMinute];
-                Serial.print(pwmVal_);
-                Serial.print("\n");
-                analogWrite(ledPin, pwmVal_);
-
-                lastAlarmMinute = time.minutes;
-                alarmMinute++;
-
-                if ( alarmMinute > ((sizeof(ledLinearity) / sizeof(ledLinearity[0]))) )
+                // Shall we turn off?
+                if ( alarmMinute >= LED_FADE_TIME )
                 {
-                    Serial.print(" alarm reset\n");
-                    alarmMinute = 0;
+                    if (alarmMinute >= LED_FADE_TIME + MINUTES_WITH_FULL_LIGHT)
+                    {
+                        Serial.print("Alarm stop at time: ");
+                        Serial.print(time.seconds);
+                        alarmMinute = 0;
 
-                    analogWrite(ledPin, 0);
-                    alarmState = alarmState_inactive;
+                        analogWrite(ledPin, 0);
+                        alarmState = alarmState_inactive;
+                    }
+                    else
+                    {
+                        lastAlarmMinute = time.minutes;
+                        alarmMinute++;
+                    }
+                }
+                else
+                {
+                    Serial.print("Alarm update at time: ");
+                    Serial.print(time.seconds);
+                    Serial.print(" new alarm minute: ");
+                    Serial.print(alarmMinute);
+                    Serial.print("\n");
+                    int pwmVal_ = ledLinearity[alarmMinute];
+                    Serial.print(pwmVal_);
+                    Serial.print("\n");
+                    analogWrite(ledPin, pwmVal_);
+
+                    lastAlarmMinute = time.minutes;
+                    lastAlarmMinute = time.seconds;
+                    alarmMinute++;
                 }
             }
             break;
